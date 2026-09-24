fe280edf5767c26f77da7ac6d14882b4ee90eac4
IrfanPatelIG <irfanpatel1997abc@gmail.com>
2026-09-25 00:02:13 +0530

    Added Table and Card view

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -43,27 +43,64 @@ function Products() {
   }
 
   return (
-    <div className='App font-sans'>
-        <nav className='flex justify-between items-center py-4 px-20 sticky top-0 bg-secondary z-10'>
+    <div className='w-screen App font-sans'>
+        <nav className='w-full flex justify-between items-center py-4 px-5 sticky top-0 bg-secondary z-10'>
             <h1 className='text-3xl font-bold '>Products</h1>
             <button className='btn-primary' onClick={handleLogout}>Logout</button>
         </nav>
 
-      {
-        products.length > 0 && (<div className='products px-20'>
-          {products.map((prod) => {
-              return <span className='product-single bg-product-card hover:bg-[#212121] hover:scale-105 hover:rounded-2xl transition-all ease-in-out' key={prod.id}>
-                  <img className='product-img' src={prod.thumbnail} alt={prod.title} />
-                  <span>{prod.title}</span>
-                </span>
-            })}
+      {products.length > 0 && (
+        <div className='w-full px-5'>
+
+            {/* Desktop: Table */}
+            <table className='hidden md:table table-fixed w-full border mx-auto my-4 border-gray-400'>
+                <thead className="h-15 bg-[#2b2b2b] w-full">
+                <th className='table-head-cell w-[7%]'>Sr. no.</th>
+                <th className='table-head-cell w-[20%]'>Image</th>
+                <th className='table-head-cell w-[30%]'>Title</th>
+                <th className='table-head-cell w-[18%]'>Category</th>
+                <th className='table-head-cell w-[12%]'>Rating</th>
+                <th className='table-head-cell w-[13%]'>Stock</th>
+                </thead>
+                <tbody className="bg-[#181818] w-full">
+                    {products.map((prod) => {
+                        return <tr className='hover:bg-[#1c1b1b] cursor-pointer w-full'>
+                            <td className='table-data-cell'>{prod.id}</td>
+                            <td className='table-data-cell'><img className='h-35 mx-auto object-cover' src={prod.thumbnail} alt={prod.title} /></td>
+                            <td className='table-data-cell'><span>{prod.title}</span></td>
+                            <td className='table-data-cell'><span>{prod.category}</span></td>
+                            <td className='table-data-cell'><span>{prod.rating}</span></td>
+                            <td className='table-data-cell'><span>{prod.stock}</span></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+
+            {/* Mobile: Cards */}
+            <div className='md:hidden flex flex-col gap-4 my-3'>
+                {products.map((prod) => {
+                    return <div className='h-fit w-[95%] mx-auto p-2 bg-product-card hover:bg-[#212121] hover:scale-105 hover:rounded-2xl transition-all ease-in-out rounded' key={prod.id}>
+                        <img className='w-full h-50 mb-2 object-contain border-b' src={prod.thumbnail} alt={prod.title} />
+                        <div className='flex gap-4 flex-col'>
+                            <div className='text-2xl'>{prod.title}</div>
+                            <div className='pl-3'><span className='text-gray-500'>Category:</span> {prod.category}</div>
+                            <div className='w-full flex gap-3 justify-between px-3'>
+                                <div><span className='text-gray-500'>Rating:</span> {prod.rating}</div>
+                                <div><span className='text-gray-500'>Stock:</span> {prod.stock}</div>
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </div>
+
         </div>)
       }
 
+
       {products.length > 0 && (
         
-        <div className='pagination bg-secondary'>
-          <div className="pagination-info">
+        <div className='w-full pagination bg-secondary'>
+          <div className="flex flex-1 items-start gap-3">
             <span>
               Showing {(page - 1) * pageSize + 1}–
               {Math.min(page * pageSize, totalProducts)} of {totalProducts}
@@ -80,14 +117,14 @@ function Products() {
             </select>
           </div>
 
-          <div className='pagination-btns'>
-            <span className={page === 1? "pagination-disabled" : ""} onClick={() => { selectedPageHandler(page-1) }}>◀</span>
+          <div className='flex flex-1 flex-wrap items-center justify-center'>
+            <span className={`${page === 1? "pagination-disabled" : ""} mx-1 page-btn`} onClick={() => { selectedPageHandler(page-1) }}>◀</span>
             {
               [...Array(totalPages)].map((e, i) => {
-                return <span className={page === i+1? "pagination-selected" : ""} onClick={() => { selectedPageHandler(i+1) }} key={i}>{i+1}</span>
+                return <span className={`${page === i+1? "pagination-selected" : ""} page-btn`} onClick={() => { selectedPageHandler(i+1) }} key={i}>{i+1}</span>
               })
             }
-            <span className={page >= totalPages? "pagination-disabled" : ""} onClick={() => { selectedPageHandler(page+1) }}>▶</span> 
+            <span className={`${page === totalPages? "pagination-disabled" : ""} mx-1 page-btn`} onClick={() => { selectedPageHandler(page+1) }}>▶</span> 
           </div>
         </div>
       )}
